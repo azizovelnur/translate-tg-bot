afcbc26015510200c364b81633898fc606fe1454
elnur <elnurwebdev@gmail.com>
2024-03-03 13:40:28 +0400

    dependencies were added

---
+++ src/main/java/com/tgTranslateBot/TgTranslateBotApplication.java
@@ -0,0 +1,13 @@
+package com.tgTranslateBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgTranslateBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgTranslateBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgTranslateBot/TgTranslateBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgTranslateBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgTranslateBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
